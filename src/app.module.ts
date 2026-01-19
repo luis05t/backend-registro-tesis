f81e7bff9802d219179d2f3dd7d5ab82c67eaebd
luis05t <148250236+luis05t@users.noreply.github.com>
2026-01-18 20:56:32 -0500

    f

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,39 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
+import { ServeStaticModule } from '@nestjs/serve-static'; // <--- IMPORTANTE
+import { join } from 'path'; 
+import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { CareersModule } from './careers/careers.module';
 import { RolesModule } from './roles/roles.module';
+import { ProjectsModule } from './projects/projects.module';
 import { PermissionsModule } from './permissions/permissions.module';
 import { RolesPermissionsModule } from './roles-permissions/roles-permissions.module';
-import { CareersModule } from './careers/careers.module';
-import { ProjectsModule } from './projects/projects.module';
-import { UsersProjectsModule } from './users-projects/users-projects.module';
 import { SkillsModule } from './skills/skills.module';
-import { PorjectsSkillsModule } from './porjects-skills/porjects-skills.module';
+import { UsersProjectsModule } from './users-projects/users-projects.module';
+import { AppController } from './app.controller'; 
+import { AppService } from './app.service';
 
 @Module({
-  imports: [UsersModule, AuthModule, RolesModule, PermissionsModule, RolesPermissionsModule, CareersModule, ProjectsModule, UsersProjectsModule, SkillsModule, PorjectsSkillsModule],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'uploads'), 
+      serveRoot: '/uploads', 
+    }),
+
+    ConfigModule.forRoot({ isGlobal: true }),
+    AuthModule,
+    UsersModule,
+    PrismaModule,
+    CareersModule,
+    RolesModule,
+    ProjectsModule,
+    PermissionsModule,
+    RolesPermissionsModule,
+    SkillsModule,
+    UsersProjectsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
