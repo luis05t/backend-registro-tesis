9e7563c895da895cfadda75adf9b857db9a9df36
luis05t <148250236+luis05t@users.noreply.github.com>
2026-01-23 13:46:14 -0500

    arreglos

--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,30 @@
 import { ValidationPipe, Logger } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
-import { NestExpressApplication } from "@nestjs/platform-express"; // 1. Importante para archivos estáticos
+import { NestExpressApplication } from "@nestjs/platform-express"; 
 import { join } from "path";
 
 async function bootstrap() {
-    // 2. Especificamos que es una aplicación de Express
     const app = await NestFactory.create<NestExpressApplication>(AppModule);
     const logger = new Logger('Bootstrap');
 
     app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
     app.setGlobalPrefix("api");
 
-    // 3. CONFIGURACIÓN PARA SERVIR IMÁGENES
-    // Esto hace que http://tu-url/uploads/foto.jpg funcione
     app.useStaticAssets(join(__dirname, '..', 'uploads'), {
         prefix: '/uploads/',
     });
 
-    // 4. CORS DINÁMICO (Optimizado)
     app.enableCors({
         origin: (origin, callback) => {
             const allowedOrigins = [
                 'http://localhost:5173',
                 'http://localhost',
                 /\.vercel\.app$/,      
-                /\.devtunnels\.ms$/,   // Permite cualquier túnel de VS Code
+                /\.devtunnels\.ms$/,   
             ];
             
-            // Si no hay origin (como apps móviles o Postman) o está en la lista blanca
+        
             if (!origin || allowedOrigins.some(pattern => 
                 typeof pattern === 'string' ? pattern === origin : pattern.test(origin)
             )) {
@@ -45,7 +41,6 @@ async function bootstrap() {
 
     const port = process.env.PORT ?? 8000;
     
-    // 5. Escuchar en 0.0.0.0 es vital para Docker y Túneles
     await app.listen(port, "0.0.0.0");
     logger.log(`Backend iniciado en puerto ${port}`);
     logger.log(`Carpeta de archivos estáticos configurada en: ${join(__dirname, '..', 'uploads')}`);
